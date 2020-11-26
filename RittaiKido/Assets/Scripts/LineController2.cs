6597f9e36dd66d27392c4164a12b515e1c4ddfee
yuta <51759549+yumarun@users.noreply.github.com>
2020-11-26 12:30:06 +0900

    ikkaime

---
+++ RittaiKido/Assets/Scripts/LineController2.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController2 : MonoBehaviour
+{
+   //TODO;
+   //弾が発射するときのlinerenderを追加する
+}
---
+++ RittaiKido/Assets/Scripts/ShootBall2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBall2 : MonoBehaviour
+{
+
+    public GameObject ballPrefab;
+    public GameObject player;
+
+
+    float shootSpeed = 5000.0f;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject ball = Instantiate(ballPrefab) as GameObject;
+            ball.transform.position = transform.position + transform.forward * 2;
+            ball.GetComponent<Rigidbody>().AddForce(transform.forward * shootSpeed);
+        }
+    }
+}
